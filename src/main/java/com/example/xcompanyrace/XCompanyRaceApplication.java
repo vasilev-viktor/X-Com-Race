9a8048c5510feb1d93b33bc3e09356e69d327250
Kingmen77 <viktor-vasilev@inbox.ru>
2024-04-04 21:12:44 +0300

    Создание и настройка проекта

---
+++ src/main/java/com/example/xcompanyrace/XCompanyRaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.xcompanyrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XCompanyRaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XCompanyRaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/xcompanyrace/XCompanyRaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xcompanyrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XCompanyRaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
